862148023399df183b864a09fb2ca85f57d3ef2b
Leonardo Forero <gerencia@micrositios.net>
2015-08-09 00:01:38 -0500

    Cambio de prueba

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,9 @@
 <?php
+
 $user = 'root';
 $pass = 'lmKuZfSr';
 $base = 'base2';
+
 try {
 	$dbh = new PDO(sprintf('mysql:charset=utf8mb4;mysql:host=localhost;dbname=%s',$base), $user, $pass);
 } catch (PDOException $e) {
